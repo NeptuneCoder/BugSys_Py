6cc15953a79c81c0a725601e5c4916d3c02c99f9
yanghai23 <GitHub163@126.com>
2017-08-18 18:16:23 +0800

    feat:新增加自动更新模块

---
+++ updateBugFlag.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# import os
+import pymysql
+
+
+def parseFile():
+    try:
+        path = "bugFlags.txt"
+
+        iter_f = iter(open(path))
+        for line in iter_f:
+            updateBugInfo(line)
+            print(line)
+    except Exception as e:
+        print("has an exception= %s"%e)
+
+def updateBugInfo(bugFlag):
+    # get db obj
+    db = pymysql.connect("localhost","root","root","bugDB")
+    cursor = db.cursor()
+    try:
+        effect_row = cursor.execute("update bugInfo set bug_status = 'fix' where bug_detail like   %s%", (bugFlag))
+        print(bugFlag+" row =  "+effect_row)
+        db.commit()
+        print('insert successed')
+    except Exception as e:
+        db.rollback()
+        print('insert failed',e)
+    db.close()
+
+parseFile()
